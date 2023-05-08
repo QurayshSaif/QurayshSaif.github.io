20d8624a5d3fbb129bb9acbadcf49442b333e09c
QurayshSaif <saifquraysh@gmaill.com>
2023-05-08 16:13:27 -0400

    added new project

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,8 +1,21 @@
 import CareerGuide from "../assets/images/portfolio/career-guide.png";
 import InStock from "../assets/images/portfolio/in-stock.png";
 import BrainFlix from "../assets/images/portfolio/brainflix.png";
+import Hulu from "../assets/images/portfolio/hulu2.png";
 
 const ProjectsPreviewData = [
+  {
+    id: "hulu",
+    img: Hulu,
+    name: "Hulu",
+    stack: ["< Next.js />", "< Tailwind CSS />", "< RESTful API />"],
+    // website link
+    src: "https://hulu-2-0-qurayshsaif.vercel.app/",
+    // github link
+    source: "https://github.com/QurayshSaif/hulu-2.0",
+    description:
+      "Hulu 2.0 is a clone website of the streaming platform Hulu, users can view the various movie or tv show categories across different screen sizes",
+  },
   {
     id: "career-guide",
     img: CareerGuide,
@@ -15,9 +28,9 @@ const ProjectsPreviewData = [
       "< Node.js />",
     ],
     // website link
-    src: "#",
+    src: "https://career-guide-qurayshsaif.vercel.app/",
     // github link
-    source: "#",
+    source: "https://github.com/QurayshSaif/career-guide",
     description:
       "Career Guide is a responsive website for users who are not sure of what career they want to pursue. This website consists of a quiz and a job-search, once the user completes the quiz the user will get a result with top 5 career suggestions based on the answers provided. The user can also do a job-search to see available jobs in that particular career field.",
   },
@@ -29,7 +42,7 @@ const ProjectsPreviewData = [
     // website link
     src: "#",
     // github link
-    source: "#",
+    source: "https://github.com/QurayshSaif/saturn-instock",
     description:
       "InStock is a responsive website to track warehouses and inventory items. Users can add, edit, delete warehouses or inventory items. Collaborated using agile methodology with a group of 4 developers on Jira. Used Gitflow to complete this project efficiently.",
   },
@@ -45,9 +58,9 @@ const ProjectsPreviewData = [
       "< Node.js />",
     ],
     // website link
-    src: "#",
+    src: "https://brainflix-qurayshsaif.vercel.app/",
     // github link
-    source: "#",
+    source: "https://github.com/QurayshSaif/BrainFlix",
     description:
       "BrainFlix is a responsive video viewing platform, where users can upload a video, add or delete a comment.",
   },
